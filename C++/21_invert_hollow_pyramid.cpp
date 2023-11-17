95ec6f102baa82ab3872533222531af031bac941
Priyanshu Maurya <priyanshumaurya.2003@gmail.com>
2023-11-17 19:33:52 +0530

    21st program completed

--- C++/21_invert_hollow_pyramid.cpp
+++ C++/21_invert_hollow_pyramid.cpp
@@ -1,6 +1,6 @@
-// ******* 
-//  *****
-//   ***
+// *     * 
+//  *   *
+//   * *
 //    * 
 #include<iostream>
 using namespace std;
@@ -9,16 +9,25 @@ int main()
   int n;
   cout<<"enter number of rows";
   cin>>n;
+  int j=n+n-1;
   for(int i=1;i<=n;i++)
   {
     for(int k=0;k<=i;k++)
     {
       cout<<' ';
     }
-   for(int j=i;j<=n;j++)
+   for(int c=j;c>0;c--)
    {
-    cout<<'*';
+    if(c==j || c==1)
+    {
+      cout<<'*';
+    }  
+    else
+    {
+      cout<<' ';
+    }
    }
+   j=j-2;
    cout<<'\n';
   }
   return 0;
